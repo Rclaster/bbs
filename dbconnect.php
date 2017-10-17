c4e60bc326690538ab38fedd43a6e05b5130e8db
HirokiTakeuchi <hiroki.t.galaxy@gmail.com>
2017-10-17 16:01:13 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 //データベースの接続と選択
-$server = "202.221.137.203";  
-$userName = "hiroki_takeuchi"; 
-$password = "Hiroki19970121"; 
-$dbName = "dev_bbs";
+$server = "";  
+$userName = ""; 
+$password = ""; 
+$dbName = "";
 
 $mysqli = new mysqli($server, $userName, $password, $dbName);
 if ($mysqli->connect_error) {
